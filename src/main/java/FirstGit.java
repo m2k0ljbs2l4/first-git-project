d76084edf82d2f5dbeb1b627997bdc5c253de80d
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-05 12:17:58 +0500

    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,5 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("LEADNING!");
+        System.out.println("It's terribly difficult!");
     }
 }
