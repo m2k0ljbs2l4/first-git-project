5a6567a2a2dfdcff41ac01861345d8f0bdb45c3a
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-05 12:31:03 +0500

    Change sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("GIT");
+        System.out.println("GIT DEVELOP");
         System.out.println("LEADNING!");
         System.out.println("It's terribly difficult!");
         System.out.println("Im happy!");
