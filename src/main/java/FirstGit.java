846a52141cd91abd4a4ecb5857c4e7c5390eb4b9
m2k0ljbs2l4 <m2k0ljbs2l4@gmail.com>
2024-11-05 12:35:34 +0500

    del sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEADNING!");
-        System.out.println("It's terribly difficult!");
         System.out.println("Im happy!");
         System.out.println("!!!!!");
     }
